deb20b02f7223742dc1a0c2d7a6370c91f581bbd
sssssoyoung <thdual924@naver.com>
2026-07-14 16:37:55 +0900

    chore: S3 버킷을 us-east-1 리전으로 재생성

--- infrastructure/create_s3.py
+++ infrastructure/create_s3.py
@@ -6,15 +6,15 @@
 #
 # [이미 생성된 리소스 - us-east-1]
 #
-#   버킷 1: pbl5-sensor-data
+#   버킷 1: pbl5team-sensor-data
 #     - 용도: IoT/센서 원시 데이터 저장
 #     - 퍼블릭 액세스 차단 적용
 #
-#   버킷 2: pbl5-knowledge-base
+#   버킷 2: pbl5team-knowledge-base
 #     - 용도: Bedrock Knowledge Base용 문서 저장
 #     - 퍼블릭 액세스 차단 적용
 #
-#   버킷 3: pbl5-output
+#   버킷 3: pbl5team-output
 #     - 용도: 분석 결과 및 출력 파일 저장
 #     - 퍼블릭 액세스 차단 적용
 #
@@ -24,9 +24,9 @@
 #      client = boto3.client('s3', region_name='us-east-1')
 #
 #   2. 버킷 3개 생성
-#      client.create_bucket(Bucket='pbl5-sensor-data')
-#      client.create_bucket(Bucket='pbl5-knowledge-base')
-#      client.create_bucket(Bucket='pbl5-output')
+#      client.create_bucket(Bucket='pbl5team-sensor-data')
+#      client.create_bucket(Bucket='pbl5team-knowledge-base')
+#      client.create_bucket(Bucket='pbl5team-output')
 #      (us-east-1은 CreateBucketConfiguration 파라미터 불필요)
 #
 #   3. 각 버킷에 퍼블릭 액세스 차단 설정
@@ -41,9 +41,9 @@
 #      )
 #
 #   4. 생성된 버킷 ARN 출력
-#      arn:aws:s3:::pbl5-sensor-data
-#      arn:aws:s3:::pbl5-knowledge-base
-#      arn:aws:s3:::pbl5-output
+#      arn:aws:s3:::pbl5team-sensor-data
+#      arn:aws:s3:::pbl5team-knowledge-base
+#      arn:aws:s3:::pbl5team-output
 #
 # [create_all.py에서 호출되는 함수]
 #   def create_s3():
