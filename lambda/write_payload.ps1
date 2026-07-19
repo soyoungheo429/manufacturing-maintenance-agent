90143d5ff40719e3446d48501bfd6eb3f2fdaf5d
sssssoyoung <thdual924@naver.com>
2026-07-19 20:45:31 +0900

    chore: agent/prompt.txt 최신 Instruction으로 갱신, 불필요 스크립트 정리

--- lambda/write_payload.ps1
+++
@@ -1,4 +0,0 @@
-$p = '{"sensor_data": [{"facility_id": "설비2", "air_temp": 310.0, "process_temp": 318.0, "rotation_speed": 1400, "torque": 122.5, "tool_wear": 235}]}'
-$enc = New-Object System.Text.UTF8Encoding($false)
-[System.IO.File]::WriteAllText("c:\Users\soyoung\Desktop\work\클라우드PBL\프로젝트\manufacturing-maintenance-agent\lambda\anomaly_filter\payload.json", $p, $enc)
-Write-Host "WRITTEN"
